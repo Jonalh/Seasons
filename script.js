e5dc060a9a464a83a07713e38994f7fc892ce48d
Jonas Haugen <jonashaugen2@gmail.com>
2021-05-02 21:41:00 +0200

    refactor js

--- script.js
+++ script.js
@@ -34,73 +34,40 @@ let ending5 = document.getElementById("ending5");
 setTimeout(function () {
   textFadeIn.remove();
   textFadeUp.remove();
-}, 13000);
-
-setTimeout(function () {
   textDance.innerHTML =
     "♪ And I must dance, and I must sing.To see the beauty of the spring ♪";
 }, 13000);
 
 setTimeout(function () {
-  textDance.remove();
-}, 26000);
+  bird.remove();
+}, 25500);
 
 setTimeout(function () {
+  textDance.remove();
   textHeatUp.innerHTML =
     "The earth is warm, the sun's ablaze, It is a time of carefree days. And bees abuzz that chance to pass.";
-}, 26000);
-
-setTimeout(function () {
   textColors1.innerHTML = "May see me snoozing in the grass. ";
-}, 26000);
-
-setTimeout(function () {
   textColors2.innerHTML = "The leaves are ";
-}, 26000);
-
-setTimeout(function () {
   textColors3.innerHTML = "yellow, ";
-}, 26000);
-
-setTimeout(function () {
   textColors4.innerHTML = "red, ";
-}, 26000);
-
-setTimeout(function () {
   textColors5.innerHTML = "and ";
-}, 26000);
-
-setTimeout(function () {
   textColors6.innerHTML = "brown. ";
-}, 26000);
-
-setTimeout(function () {
   textColors7.innerHTML = "A shower sprinkle softly down.";
 }, 26000);
 
 setTimeout(function () {
+  leaves.remove();
+}, 35000);
+
+setTimeout(function () {
   textHeatUp.remove();
-}, 39000);
-
-setTimeout(function () {
   textColors.remove();
-}, 39000);
-
-setTimeout(function () {
   textCool1.innerHTML = "The air is fragrant, crisp, and ";
-}, 39000);
-
-setTimeout(function () {
   textCool2.innerHTML = "cool, ";
-}, 39000);
-
-setTimeout(function () {
   textCool3.innerHTML = "And once again I'm stuck in school.";
-}, 39000);
-
-setTimeout(function () {
   textBlowAway.innerHTML =
     "The birds are gone, the world is white, The winds are wild, they chill and bite.";
+  bees.remove();
 }, 39000);
 
 setTimeout(function () {
@@ -111,30 +78,11 @@ setTimeout(function () {
 }, 52000);
 
 setTimeout(function () {
-  bird.remove();
-}, 25500);
-
-setTimeout(function () {
   thickWinter1.innerHTML = "The ground is";
-}, 55000);
-
-setTimeout(function () {
   thickWinter2.innerHTML = "thick";
-}, 55000);
-
-setTimeout(function () {
   thickWinter3.innerHTML = "with slush and sleet, And I can";
-}, 55000);
-
-setTimeout(function () {
   thickWinter4.innerHTML = "barely";
-}, 55000);
-
-setTimeout(function () {
   thickWinter5.innerHTML = "feel my feet.";
-}, 55000);
-
-setTimeout(function () {
   lastWinter.innerHTML =
     "The last is done, the next is here,The same as it is every year.";
 }, 55000);
@@ -146,11 +94,3 @@ setTimeout(function () {
   ending4.innerHTML = "snow";
   ending5.innerHTML = "That is how each year must go.";
 }, 60000);
-
-setTimeout(function () {
-  leaves.remove();
-}, 35000);
-
-setTimeout(function () {
-  bees.remove();
-}, 39000);
